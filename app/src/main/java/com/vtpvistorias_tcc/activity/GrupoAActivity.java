0914506593174733c62338d6f1115211b925aec9
Caroline Fns <carolineadm.santos@gmail.com>
2018-06-03 02:47:08 -0300

    6.1° Versão
    melhoramento tela checklist

--- app/src/main/java/com/vtpvistorias_tcc/activity/GrupoAActivity.java
+++ app/src/main/java/com/vtpvistorias_tcc/activity/GrupoAActivity.java
@@ -1,14 +1,36 @@
 package com.vtpvistorias_tcc.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.vtpvistorias_tcc.R;
 
-public class GrupoAActivity extends AppCompatActivity {
+public class GrupoAActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button botaoVoltarSelecionarFicha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grupo_a);
+
+        botaoVoltarSelecionarFicha = (Button) findViewById(R.id.botaoVoltarSelecionarFicha);
+        botaoVoltarSelecionarFicha.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+
+            case R.id.botaoVoltarSelecionarFicha:
+                finish();
+                break;
+
+
+        }
+
     }
 }
