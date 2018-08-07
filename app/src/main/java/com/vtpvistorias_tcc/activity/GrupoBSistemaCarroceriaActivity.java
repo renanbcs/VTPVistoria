39ac98315fffb68d480cc383c7f858bf5c991233
GabrielRossi <gabriel.5567@hotmail.com>
2018-08-07 20:33:47 -0300

    7.10ºVersão
    Explicação Max.

---
+++ app/src/main/java/com/vtpvistorias_tcc/activity/GrupoBSistemaCarroceriaActivity.java
@@ -0,0 +1,15 @@
+package com.vtpvistorias_tcc.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.vtpvistorias_tcc.R;
+
+public class GrupoBSistemaCarroceriaActivity extends AppCompatActivity {
+
+    
+}
